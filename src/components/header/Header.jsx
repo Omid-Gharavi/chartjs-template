0d3c01fe04aca104208ee23c39f8f2317f65c943
OMID GHARAVI <omidbros3@gmail.com>
2024-05-08 23:42:50 +0330

    Component: Header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import { Card, CardBody } from '@nextui-org/react'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <Card className='rounded-none'>
+                <CardBody className='flex flex-row justify-between items-center'>
+                    <div>
+                        {/* <img src="" alt="" /> Logo Image */}
+                        <p className='text-2xl font-bold'>Logo</p>
+                    </div>
+                    <ul className='flex gap-4'>
+                        <li>محصولات</li>
+                        <li>تماس با ما</li>
+                        <li>درباره ما</li>
+                    </ul>
+                    <div>
+                        <p>Signin / Logout</p>
+                    </div>
+                </CardBody>
+            </Card>
+        </>
+    )
+}
+
+export default Header
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import Header from "@/components/header/Header";
 import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  return (
+    <NextUIProvider>
+      <Header />
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
