882aef56a96a09290fbc4f8b110f89e370198c24
OMID GHARAVI <omidbros3@gmail.com>
2024-05-13 14:15:14 +0330

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,42 @@
+import { Card, CardBody } from '@nextui-org/react'
 import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <Card className='rounded-none'>
+            <CardBody>
+                <div className='py-10 flex max-xl:flex-col max-xl:items-center flex-row-reverse flex-wrap justify-center gap-10'>
+                    <div className='mr-11 max-xl:mr-[unset]'>
+                        {/* <img src="" alt="" /> */}
+                        <p className='font-bold text-2xl'>Logo</p>
+                    </div>
+                    <ul>
+                        <li>Click</li>
+                        <div className='flex flex-col gap-1 mt-4'>
+                            <li>Home</li>
+                            <li>Contact</li>
+                            <li>About</li>
+                        </div>
+                    </ul>
+                    <ul>
+                        <li>Click</li>
+                        <div className='flex flex-col gap-1 mt-4'>
+                            <li>Home</li>
+                            <li>Contact</li>
+                            <li>About</li>
+                        </div>
+                    </ul>
+                    <ul>
+                        <li>Click</li>
+                        <div className='flex flex-col gap-1 mt-4'>
+                            <li>Home</li>
+                            <li>Contact</li>
+                            <li>About</li>
+                        </div>
+                    </ul>
+                </div>
+            </CardBody>
+        </Card>
     )
 }
 
